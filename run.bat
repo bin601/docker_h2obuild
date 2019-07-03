1349fa3d21806838588a4bfc7094db274f410ef1
bin.lain <bin_601@mail2000.com.tw>
2019-07-03 16:59:19 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,40 @@
-docker run  -it --rm  -v d:\svn:/opt bin601/h2obuild  /bin/bash
+@echo off
+setlocal enableextensions 
+
+set BASE_IMAGE=bin601/h2obuild
+set CONTAINER=%USERNAME%_h2obuild_app 
+set START_SHELL=/bin/bash 
+
+set HASH=
+set HASH_STOPPED=
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -q -f name^=%CONTAINER%`) do ( 
+    SET HASH=%%F
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -qa -f name^=%CONTAINER%`) do ( 
+    SET HASH_STOPPED=%%F
+)
+
+if not "%HASH%" == "" (
+    echo "founding existing running container %CONTAINER%, proceed to exec another shell"
+    docker exec -it %HASH% %START_SHELL%
+    goto :EOF
+) 
+
+if not "%HASH_STOPPED%" == "" (
+    echo "founding existing stopped container %CONTAINER%, proceed to start"
+    docker start --attach -i %HASH_STOPPED%
+    goto :EOF
+) else (
+    echo "existing container not found, createing a new one, named %CONTAINER%"
+    docker run --name=%CONTAINER% --hostname=%CONTAINER% -it  -v d:\svn:/opt  %BASE_IMAGE% %START_SHELL%
+)
+
+
+
+
+
+
+
+REM set HASH=
+REM docker run  -it --rm  -v d:\svn:/opt bin601/h2obuild  /bin/bash
