70714e9e3461c39336c7745ddc53eb0d5a0e865a
bin.lain <bin_601@mail2000.com.tw>
2019-07-03 17:38:43 +0800

    Add "run new"

--- run.bat
+++ run.bat
@@ -4,15 +4,19 @@ setlocal enableextensions
 set BASE_IMAGE=bin601/h2obuild
 set CONTAINER=%USERNAME%_h2obuild_app 
 set START_SHELL=/bin/bash 
+set SOURCE_FOLDER=d:\svn
 
-set HASH=
-set HASH_STOPPED=
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -q -f name^=%CONTAINER%`) do ( 
     SET HASH=%%F
 )
 FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -qa -f name^=%CONTAINER%`) do ( 
     SET HASH_STOPPED=%%F
+)
+
+if "%1" == "new" (
+  docker run -it --rm -v %SOURCE_FOLDER%:/opt  %BASE_IMAGE% %START_SHELL%
+  goto :EOF
 )
 
 if not "%HASH%" == "" (
@@ -27,13 +31,8 @@ if not "%HASH_STOPPED%" == "" (
     goto :EOF
 ) else (
     echo "existing container not found, createing a new one, named %CONTAINER%"
-    docker run --name=%CONTAINER% --hostname=%CONTAINER% -it  -v d:\svn:/opt  %BASE_IMAGE% %START_SHELL%
+    docker run --name=%CONTAINER% --hostname=%CONTAINER% -it  -v %SOURCE_FOLDER%:/opt  %BASE_IMAGE% %START_SHELL%
 )
-
-
-
-
-
 
 
 REM set HASH=
