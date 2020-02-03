de086e2053f609602c4e533dc531d627deb19d19
bin.lain <bin_601@mail2000.com.tw>
2020-02-03 14:57:07 +0800

    change to docker run from docker-compose

--- run.bat
+++ run.bat
@@ -1,39 +1,67 @@
 @echo off
-setlocal enableextensions 
-
-set BASE_IMAGE=bin601/h2obuild
-set CONTAINER=%USERNAME%_h2obuild_app 
-set START_SHELL=/bin/bash 
-set SOURCE_FOLDER=d:\svn
 
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -q -f name^=%CONTAINER%`) do ( 
-    SET HASH=%%F
-)
-FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -qa -f name^=%CONTAINER%`) do ( 
-    SET HASH_STOPPED=%%F
-)
+set SMB_NAME=smb
+set RUNNING_HASH=
+set STOPPED_HASH=
 
-if "%1" == "new" (
-  docker run -it --rm -v %SOURCE_FOLDER%:/opt  %BASE_IMAGE% %START_SHELL%
-  goto :EOF
+
+REM
+REM exist a running samba server (container)
+REM
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -q -f name^=^^%SMB_NAME%$`) do ( 
+    goto start 
 )
 
-if not "%HASH%" == "" (
-    echo "founding existing running container %CONTAINER%, proceed to exec another shell"
-    docker exec -it %HASH% %START_SHELL%
-    goto :EOF
-) 
-
-if not "%HASH_STOPPED%" == "" (
-    echo "founding existing stopped container %CONTAINER%, proceed to start"
-    docker start --attach -i %HASH_STOPPED%
-    goto :EOF
-) else (
-    echo "existing container not found, createing a new one, named %CONTAINER%"
-    docker run --name=%CONTAINER% --hostname=%CONTAINER% -it  -v %SOURCE_FOLDER%:/opt -v samba-public:/opt2 %BASE_IMAGE% %START_SHELL% 
+REM
+REM exist a stopped samba server (container) 
+REM
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker ps -qa -f name^=^^%SMB_NAME%$`) do ( 
+    docker restart %SMB_NAME%
+    goto start 
 )
 
+REM
+REM create samba-public volume
+REM
+echo create samba-public volume
+docker volume create --name samba-public > nul
 
-REM set HASH=
-REM docker run  -it --rm  -v d:\svn:/opt bin601/h2obuild  /bin/bash
+REM
+REM create samba service
+REM
+echo create samba service
+docker run --name %SMB_NAME% -v samba-public:/mnt/public -d dperson/samba  -u "user;password" -g "aio read size = 0" -g "aio write size = 0" -r -s "public;/mnt/public;yes;no;no;all;user" > nul 
+
+:start
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" smb`) DO (
+SET IP=%%F
+)
+echo.
+echo smaba information 
+echo   IP:%IP% 
+echo   UserName: user
+echo   Password: password
+echo.
+echo.
+echo If you can't connect %IP%, you must key in below in command window with Administrator right firstly
+echo.
+echo route -p add 172.17.0.0 mask 255.255.255.0 10.0.75.2
+echo.
+
+REM
+REM if you want map share windows folder, you can change SHARDED_FOLDER
+REM
+
+set SHARDED_FOLDER=d:\svn
+
+if exist %SHARDED_FOLDER% set SHARDED_CMD=-v %SHARDED_FOLDER%:/opt2
+docker run  -it --rm --workdir=/opt -v samba-public:/opt %SHARDED_CMD% bin601/h2obuild /bin/bash
+
+
+
+
+
+
+
