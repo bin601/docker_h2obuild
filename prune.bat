bc8c5c44c36f86fd839fc6df8251e19f31d682a4
bin.lain <bin_601@mail2000.com.tw>
2020-02-04 17:05:13 +0800

    change wait time to 1s

--- prune.bat
+++ prune.bat
@@ -1,4 +1,4 @@
 FOR /f "tokens=*" %%i IN ('docker ps -aq -f ancestor^=bin601/h2obuild') DO docker rm -f %%i 
 
-REM wait 5s to close dialog 
-@ping 127.0.0.1 -n 5 -w 1000 > nul
+REM wait 1s to close dialog 
+@ping 127.0.0.1 -n 1 -w 1000 > nul
